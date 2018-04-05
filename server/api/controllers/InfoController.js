43745b1573e7f409946743e10a26c5d6cd9dec45
lucas.henrique <lucas.henrique@viasoft.com.br>
2018-04-05 17:58:52 -0300

    adicionado models

---
+++ server/api/controllers/InfoController.js
@@ -0,0 +1,11 @@
+/**
+ * InfoController
+ *
+ * @description :: Server-side logic for managing infoes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
